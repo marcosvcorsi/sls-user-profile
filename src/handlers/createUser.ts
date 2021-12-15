c817e852522fe03ce0cfa7f1f27db2e2aa94cbc5
Marcos Corsi <marcosvcorsi@gmail.com>
2021-12-15 00:26:50 -0300

    chore: add eslint and prettier

--- src/handlers/createUser.ts
+++ src/handlers/createUser.ts
@@ -1,8 +1,6 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyHandler } from 'aws-lambda';
 
-export const handler: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true }),
-  };
-};
+export const handler: APIGatewayProxyHandler = async () => ({
+  statusCode: 200,
+  body: JSON.stringify({ ok: true }),
+});
